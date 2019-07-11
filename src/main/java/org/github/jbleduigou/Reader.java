884d5f269a36363fd2b4c6aa4df4bb631fdf85ea
Jean-Baptiste Le Duigou <jb.leduigou@gmail.com>
2019-07-11 18:05:59 +0200

    Address CVE-2019-0228

--- src/main/java/org/github/jbleduigou/Reader.java
+++ src/main/java/org/github/jbleduigou/Reader.java
@@ -1,14 +1,15 @@
 package org.github.jbleduigou;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.util.PDFTextStripper;
+import org.apache.pdfbox.text.PDFTextStripper;
 
+import java.io.File;
 import java.io.IOException;
 
 class Reader {
 
   String read(String filename) throws IOException {
-    PDDocument document = PDDocument.load(filename);
+    PDDocument document = PDDocument.load(new File(filename));
     String text = (new PDFTextStripper()).getText(document);
     document.close();
     return text;
